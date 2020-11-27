281598ab943c6bb46398932366210e572c6d46af
kevinsee15 <34367741+kevinsee15@users.noreply.github.com>
2020-11-27 11:09:03 +0800

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,29 @@ metadata = {
   displayName: "K2 JSSP Dynamic Service Object",
   description: "An example broker dynamically generates service objects.",
   configuration: {
+    "Web API URL": {
+     "displayName": "Web API URL",
+     "type": "string",
+     required: true
+    },
     "Columns": {
      "displayName": "Columns (Comma Separated)",
+     "type": "string",
+     required: true
+    },
+    "Has Header": {
+     "displayName": "Has Header",
+     "type": "boolean",
+     required: true
+    },
+    "Skip Row Count": {
+     "displayName": "Skip Row Count",
+     "type": "number"
+    },
+    "Sheet Name": {
+     "displayName": "Sheet Name",
      "type": "string"
-    }
+    },
   }
 };
 
@@ -18,20 +37,35 @@ ondescribe = async function ({ configuration }): Promise<void> {
 
   var schema = {
     objects: {
-      "CSVReader" : {
-        displayName: "CSV Reader",
-        description: "Reads a CSV file and returns the contents as a list.",
-        properties: {}
+      "ExcelCSVReader" : {
+        displayName: "Excel or CSV Reader",
+        description: "Reads an Excel or CSV file and returns the contents as a list.",
+        properties: {
+          "File" : {
+            displayName: "File",
+            type: "attachment"
+          }
+        },
+        methods: {
+          Read : {
+            displayName: "Read",
+            type: "list",
+            inputs:["File"],
+            outputs:[]
+          }
+        }
       }
     }
   }
 
   for (let column of columns) {
-    schema.objects.CSVReader.properties[column] = {
+    schema.objects.ExcelCSVReader.properties[column] = {
       displayName: column,
       type: "string"
     };
   }
+
+  schema.objects.ExcelCSVReader.methods.Read.outputs = columns;
 
   postSchema(schema);
 };
@@ -45,74 +79,44 @@ onexecute = async function ({
   schema,
 }): Promise<void> {
   switch (objectName) {
-    case "todo":
-      await onexecuteTodo(methodName, properties, parameters);
+    case "ExcelCSVReader":
+      await onexecute_ExcelCSVReader(methodName, properties, parameters);
       break;
     default:
       throw new Error("The object " + objectName + " is not supported.");
   }
 };
 
-async function onexecuteTodo(
+async function onexecute_ExcelCSVReader(
   methodName: string,
   properties: SingleRecord,
   parameters: SingleRecord
 ): Promise<void> {
   switch (methodName) {
-    case "get":
-      await onexecuteTodoGet(properties);
-      break;
-    case "getParams":
-      await onexecuteTodoGetWithParams(parameters);
+    case "Read":
+      await onexecute_ExcelCSVReader_Read(properties);
       break;
     default:
       throw new Error("The method " + methodName + " is not supported.");
   }
 }
 
-function onexecuteTodoGet(properties: SingleRecord): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      try {
-        if (xhr.readyState !== 4) return;
-        if (xhr.status !== 200)
-          throw new Error("Failed with status " + xhr.status);
-
-        var obj = JSON.parse(xhr.responseText);
-        postResult({
-          id: obj.id,
-          userId: obj.userId,
-          title: obj.title,
-          completed: obj.completed,
-        });
-        resolve();
-      } catch (e) {
-        reject(e);
-      }
-    };
-
-    if (typeof properties["id"] !== "number")
-      throw new Error('properties["id"] is not of type number');
-    xhr.open(
-      "GET",
-      "https://jsonplaceholder.typicode.com/todos/" +
-        encodeURIComponent(properties["id"])
-    );
-    xhr.setRequestHeader("test", "test value");
-    xhr.send();
-  });
-}
-
-function onexecuteTodoGetWithParams(parameters: SingleRecord): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    try {
-      postResult({
-        id: parameters["pid"],
-      });
-      resolve();
-    } catch (e) {
-      reject(e);
+function onexecute_ExcelCSVReader_Read(properties: SingleRecord) {
+  // Call Web API Here
+  postResult([
+    {
+      "Full Name": "John Doe",
+      "Age": "29",
+      "Birthdate": "11/12/1992",
+      "Birth Place": "Singapore, Singapore",
+      "Contact Number": "65 8888 1111"
+    },
+    {
+      "Full Name": "Jane Doe",
+      "Age": "27",
+      "Birthdate": "01/08/1992",
+      "Birth Place": "Johor, Malaysia",
+      "Contact Number": "65 1111 8888"
     }
-  });
+  ]);
 }
